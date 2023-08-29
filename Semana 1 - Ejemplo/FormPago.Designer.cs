ceed2ad1b3f816e5b75164c13bc2393260d5b95f
Alexandra <alexa.dayanna07@gmail.com>
2023-08-29 02:25:41 -0500

    cambiar nombre del label total

--- Semana 1 - Ejemplo/FormPago.Designer.cs
+++ Semana 1 - Ejemplo/FormPago.Designer.cs
@@ -223,17 +223,17 @@
             this.label5.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
             this.label5.Font = new System.Drawing.Font("Artifakt Element", 9.749999F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label5.ForeColor = System.Drawing.Color.Black;
-            this.label5.Location = new System.Drawing.Point(630, 406);
+            this.label5.Location = new System.Drawing.Point(546, 406);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(47, 21);
+            this.label5.Size = new System.Drawing.Size(135, 21);
             this.label5.TabIndex = 12;
-            this.label5.Text = "Total :";
+            this.label5.Text = "Total de empleados :";
             // 
             // labelTotal
             // 
             this.labelTotal.AutoSize = true;
             this.labelTotal.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.labelTotal.Location = new System.Drawing.Point(695, 409);
+            this.labelTotal.Location = new System.Drawing.Point(699, 409);
             this.labelTotal.Name = "labelTotal";
             this.labelTotal.Size = new System.Drawing.Size(0, 16);
             this.labelTotal.TabIndex = 13;
